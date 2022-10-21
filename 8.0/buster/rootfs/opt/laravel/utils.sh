d7620266b26d9eb637fe247cea72ddfdbc3a23ce
thphuong <thphuong1093@gmail.com>
2022-10-21 17:30:20 +0700

    fix: laravel init database step
    - Fix wrong nc arguments
    - Fix useless if condition
    - Option to skip migrations

--- 8.0/buster/rootfs/opt/laravel/utils.sh
+++ 8.0/buster/rootfs/opt/laravel/utils.sh
@@ -34,6 +34,39 @@ has_role() {
     fi
 }
 
-is_standalone() {
+is_full_mode() {
     has_role "server" && has_role "worker" && has_role "cron"
+}
+
+make_writable() {
+    chown :www-data "$1"
+    chmod g+w "$1"
+}
+
+wait_for() {
+    local host=$1
+    local port=$2
+    local timeout=$3
+    local status=0
+
+    timeout ${timeout:-30} sh -c "while ! nc -z $host $port; do sleep 1; done" 2>/dev/null || status=$?
+
+    if [ "$status" = 143 ]; then
+        return 124
+    fi
+
+    return $status
+}
+
+info() {
+    echo "info: $@"
+}
+
+warn() {
+    echo "warn: $@"
+}
+
+fatal() {
+    echo "fatal: $@"
+    exit 1
 }
